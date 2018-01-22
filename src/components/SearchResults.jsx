de0895f6e2d7d5417e4b1f28888d7ab4be94605b
Ben Cloutier <cloutier.benjamin@gmail.com>
2018-01-22 00:46:00 -0600

    only replace ending hiragana

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -54,25 +54,25 @@ export default class SearchResults extends React.Component {
                 let ret;
                 switch (verb.type) {
                   case 'v1':
-                    ret = verb.val + ' => ' + verb.val.replace('る','ます'); break;
+                    ret = verb.val + ' => ' + verb.val.replace(/る$/,'ます'); break;
                   case 'v5b':
-                    ret = verb.val + ' => ' + verb.val.replace('ぶ','びます'); break;
+                    ret = verb.val + ' => ' + verb.val.replace(/ぶ$/,'びます'); break;
                   case 'v5g':
-                    ret = verb.val + ' => ' + verb.val.replace('ぐ','ぎます'); break;
+                    ret = verb.val + ' => ' + verb.val.replace(/ぐ$/,'ぎます'); break;
                   case 'v5k':
-                    ret = verb.val + ' => ' + verb.val.replace('く','きます'); break;
+                    ret = verb.val + ' => ' + verb.val.replace(/く$/,'きます'); break;
                   case 'v5m':
-                    ret = verb.val + ' => ' + verb.val.replace('む','みます'); break;
+                    ret = verb.val + ' => ' + verb.val.replace(/む$/,'みます'); break;
                   case 'v5n':
-                    ret = verb.val + ' => ' + verb.val.replace('ぬ','にます'); break;
+                    ret = verb.val + ' => ' + verb.val.replace(/ぬ$/,'にます'); break;
                   case 'v5r':
-                    ret = verb.val + ' => ' + verb.val.replace('る','ります'); break;
+                    ret = verb.val + ' => ' + verb.val.replace(/る$/,'ります'); break;
                   case 'v5s':
-                    ret = verb.val + ' => ' + verb.val.replace('す','します'); break;
+                    ret = verb.val + ' => ' + verb.val.replace(/す$/,'します'); break;
                   case 'v5t':
-                    ret = verb.val + ' => ' + verb.val.replace('つ','ちます'); break;
+                    ret = verb.val + ' => ' + verb.val.replace(/つ$/,'ちます'); break;
                   case 'v5u':
-                    ret = verb.val + ' => ' + verb.val.replace('う','います'); break;
+                    ret = verb.val + ' => ' + verb.val.replace(/う$/,'います'); break;
                   default: break;
                 }
                 return (<li>{ret}</li>);
